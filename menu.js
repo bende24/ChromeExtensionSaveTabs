14ae09aaeb063c9b3f08a5c92d7415da01de3c5d
Bende <bendr424@gmail.com>
2020-08-17 21:22:40 +0200

    Code refactoring
    - Refactored code into a more object oriented architecture.
    - Implemented tab deletion (naive)
    Known warning:
    - Favicons cause cookie SameSite warning

--- menu.js
+++ menu.js
@@ -1,145 +1,197 @@
-chrome.storage.local.get({ tabs: [] }, function (data) {
-	let background = chrome.extension.getBackgroundPage();
-
+document.addEventListener("DOMContentLoaded", function (event) {
 	function $(a) {
 		return document.getElementById(a);
 	}
 
-	let temp = {
-		tabs: [
-			{
-				id: 0,
-				favicon:
-					"https://www.youtube.com/s/desktop/5718b32a/img/favicon_32.png",
-				url:
-					"https://www.youtube.com/watch?v=qDt-J0yHMS0&list=PLPdkBLbDPtqoHDcjUweIJqe6GKnOz0-Qw",
-				name: "Computational Astrophysics",
-			},
-			{
-				id: 1,
-				favicon:
-					"https://www.youtube.com/s/desktop/5718b32a/img/favicon_32.png",
-				url:
-					"https://www.youtube.com/watch?v=PzwxxZJpb98&list=PLu02O8xRZn7xtNx03Rlq6xMRdYcQgEpar",
-				name: "Computational Neuroscience",
-			},
-		],
-	};
+	class App {
+		constructor(data) {
+			this.data = data;
 
-	function deleteTabFromTable(tab){
-		tab.parentNode.removeChild(tab);
-	}
+			this.logic = new Logic();
+			console.log(this.logic);
 
-	function createTab(url){
-		chrome.tabs.create({
-			url: url
-		});
-	}
+			this.add = $("add");
+			this.saveSession = $("save-session");
+			this.loadSession = $("load-session");
+			this.clear = $("clear");
+			this.tabContainer = $("tab-container");
 
-	function saveCurrentData() {
-		chrome.storage.local.set({ tabs: data.tabs });
-	}
+			this.initEvents();
+			this.initTable();
+		}
 
-	function deleteTab(id) {
-		data.tabs.splice(id, 1);
-		saveCurrentData();
-	}
+		initTable() {
+			console.log(this.data.tabs);
+			this.addTabsToTable(this.data.tabs);
+		}
 
-	function addTabToTable(tab, id) {
-		const { name, url, favicon } = tab;
+		addTabsToTable(tabs) {
+			for (let tab of tabs) {
+				this.addTabToTable(tab);
+			}
+		}
 
-		let tabDiv = document.createElement("div");
-		tabDiv.classList.add("tab");
-		$("tab-container").appendChild(tabDiv);
+		addTabToTable(tab) {
+			const { name, url, favicon } = tab;
 
-		let tabFavicon = new Image();
-		tabFavicon.src = favicon;
-		tabFavicon.className = "tab-favicon";
-		tabDiv.appendChild(tabFavicon);
+			let tabDiv = document.createElement("div");
+			tabDiv.classList.add("tab");
+			this.tabContainer.appendChild(tabDiv);
 
-		let tabLabel = document.createElement("a");
-		tabLabel.setAttribute("href", url);
-		tabLabel.className = "tab-label";
-		tabLabel.innerHTML = name;
-		tabDiv.appendChild(tabLabel);
-		tabLabel.onclick = function () {
-			createTab(url);
-		};
+			let tabFavicon = new Image();
+			tabFavicon.src = favicon;
+			tabFavicon.className = "tab-favicon";
+			tabDiv.appendChild(tabFavicon);
 
-		let tabDelete = document.createElement("button");
-		tabDelete.setAttribute("data-id", id);
-		tabDelete.classList.add("btn", "btn-danger", "tab-delete");
-		tabDelete.innerHTML = "X";
-		tabDiv.appendChild(tabDelete);
-		tabDelete.onclick = function () {
-			deleteTab(tabDelete.dataset.id);
-			deleteTabFromTable(tabDiv);
-		};
-	}
+			let tabLabel = document.createElement("a");
+			tabLabel.setAttribute("href", url);
+			tabLabel.className = "tab-label";
+			tabLabel.innerHTML = name;
+			tabDiv.appendChild(tabLabel);
+			tabLabel.onclick = () => {
+				this.logic.createTab(url);
+			};
 
-	function addTabsToTable(tabs) {
-		let id = 0;
-		for (tab of tabs) {
-			addTabToTable(tab, id);
-			id++;
+			let tabDelete = document.createElement("button");
+			tabDelete.setAttribute("data-id", tab.id);
+			tabDelete.classList.add("btn", "btn-danger", "tab-delete");
+			tabDelete.innerHTML = "X";
+			tabDiv.appendChild(tabDelete);
+			tabDelete.onclick = () => {
+				this.logic.deleteTab(tabDelete.dataset.id, this.data.tabs);
+				this.deleteTabFromTable(tabDiv);
+			};
+		}
+
+		deleteTabFromTable(tab) {
+			tab.parentNode.removeChild(tab);
+		}
+
+		initEvents() {
+			this.add.addEventListener("click", () => {
+				chrome.tabs.query(
+					{ currentWindow: true, active: true },
+					(tabs) => {
+						let tab = {
+							id: this.data.tabs[this.data.tabs.length - 1].id + 1,
+							favicon: tabs[0].favIconUrl,
+							url: tabs[0].url,
+							name:
+								tabs[0].title.length > 37
+									? tabs[0].title.substring(0, 37) + "..."
+									: tabs[0].title,
+						};
+
+						console.log(tab);
+
+						this.addTabToTable(tab);
+						this.logic.saveTab(tab, this.data.tabs);
+					},
+				);
+			});
+
+			this.saveSession.addEventListener("click", () => {
+				chrome.tabs.query({}, (tabs) => {
+					chrome.storage.local.set({ session: tabs });
+					this.logic.createTab();
+					for (tab of tabs) {
+						chrome.tabs.remove(tab.id);
+					}
+				});
+			});
+
+			this.loadSession.addEventListener("click", () => {
+				chrome.storage.local.get({ session: [] }, (data) => {
+					for (tab of data.session) {
+						this.logic.createTab(tab.url);
+					}
+				});
+			});
+
+			this.clear.addEventListener("click", () => {
+				if (confirm("Are you sure you want to delete the cache?")) {
+					chrome.storage.local.clear(function () {});
+					console.log("Local cache deleted");
+				}
+			});
 		}
 	}
 
-	function saveTab(tab) {
-		data.tabs.push(tab);
-		background.write(data);
-		saveCurrentData();
-	}
+	class Logic {
+		createTab(url) {
+			chrome.tabs.create({
+				url: url,
+			});
+		}
 
-	function initTabs() {
-		let data = temp.tabs;
-		chrome.storage.local.set({ tabs: data });
-	}
-
-	function initTable() {
-		addTabsToTable(data.tabs);
-	}
-	//initTabs();
-	initTable();
-
-	$("add").addEventListener("click", () => {
-		chrome.tabs.query({ currentWindow: true, active: true }, function (
-			tabs,
-		) {
-			let tab = {
-				favicon: tabs[0].favIconUrl,
-				url: tabs[0].url,
-				name:
-					tabs[0].title.length > 37
-						? tabs[0].title.substring(0, 37) + "..."
-						: tabs[0].title,
-			};
-
-			addTabToTable(tab);
-			saveTab(tab);
-		});
-	});
-
-	$("save_session").addEventListener("click", () => {
-		chrome.tabs.query({}, function (tabs) {
-			chrome.storage.local.set({ session: tabs });	
-			createTab();
-			for(tab of tabs){
-				chrome.tabs.remove(tab.id);
+		deleteTab(id, tabs) {
+			id = parseInt(id);
+			const index = tabs.indexOf(tabs.find((e) => e.id == id));
+			if (index > -1) {
+				tabs.splice(index, 1);
 			}
-		});
-	});
+			console.log({ index: index, id: id, tabs: tabs });
+			this.saveCurrentData(tabs);
+		}
 
-	$("load_session").addEventListener("click", () => {
-		chrome.storage.local.get({ session: [] }, function (data) {
-			for(tab of data.session){
-				createTab(tab.url);
-			}
-		});
-	});
+		saveTab(tab, tabs) {
+			tabs.push(tab);
+			this.saveCurrentData(tabs);
+		}
 
-	//TESTING PURPOSE ONLY
-	$("clear").addEventListener("click", () => {
-		chrome.storage.local.clear(function () {});
+		saveCurrentData(tabs) {
+			chrome.storage.local.set({ tabs: tabs });
+		}
+	}
+
+	chrome.storage.local.get({ tabs: [] }, function (data) {
+		let temp = {
+			tabs: [
+				{
+					id: 0,
+					favicon:
+						"https://www.youtube.com/s/desktop/5718b32a/img/favicon_32.png",
+					url:
+						"https://www.youtube.com/watch?v=qDt-J0yHMS0&list=PLPdkBLbDPtqoHDcjUweIJqe6GKnOz0-Qw",
+					name: "Computational Astrophysics",
+				},
+				{
+					id: 1,
+					favicon:
+						"https://www.youtube.com/s/desktop/5718b32a/img/favicon_32.png",
+					url:
+						"https://www.youtube.com/watch?v=PzwxxZJpb98&list=PLu02O8xRZn7xtNx03Rlq6xMRdYcQgEpar",
+					name: "Computational Neuroscience",
+				},
+				{
+					id: 2,
+					favicon: "https://devstronomy.com/rocket.png",
+					name: "Devstronomy",
+					url: "https://devstronomy.com/#/",
+				},
+				{
+					id: 3,
+					favicon:
+						"http://www.astropython.org/static/images/favicons/favicon.ico",
+					name: "Astropython",
+					url: "http://www.astropython.org/",
+				},
+				{
+					id: 4,
+					favicon:
+						"",
+					name: "NASA Open APIs",
+					url: "https://api.nasa.gov/",
+				},
+			],
+		};
+
+		function initTabs() {
+			chrome.storage.local.set({ tabs: temp.tabs });
+		}
+
+		//initTabs();
+
+		let app = new App(data);
 	});
 });
